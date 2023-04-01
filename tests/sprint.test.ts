99eb6d4b9b75928b26b9fac6a4644a8b52a7810a
Melvin <Melvingiebels@gmail.com>
2023-04-01 15:14:16 +0000

    Make coverage better

--- tests/sprint.test.ts
+++ tests/sprint.test.ts
@@ -28,8 +28,10 @@ import { TestJob } from '../src/Jobs/TestJob';
 import { Developer } from '../src/Roles/Developer';
 import { FinishedState } from '../src/Sprint/FinishedState';
 import { Job } from '../src/Jobs/Job';
+import { SprintBuilder } from '../src/Sprint/SprintBuilder';
 
-describe("Sprint", () => {
+describe("Release sprint", () => {
+  let id: string;
   let productOwner: Person<ProductOwner>
   let scrumMaster: Person<ScrumMaster>;
   let tester: Person<Tester>;
@@ -58,6 +60,7 @@ describe("Sprint", () => {
 
     name = "Sprint 1";
     type = SprintType.Release;
+    backlog = new SprintBacklogFactory().create(new Repository("Project", "Master"))
 
     pipelineJobs = [
       new InstallPackagesJob(),
@@ -73,25 +76,98 @@ describe("Sprint", () => {
     .addName(name)
     .addMembers([developer, tester])
     .addType(type)
-    .addSprintBackLog(new SprintBacklogFactory().create(new Repository("Project", "Master")))
+    .addSprintBackLog(backlog)
     .addPipelineJobs(pipelineJobs)
     .build();
+
+    id = sprint.getId();
   });
 
-  test("sprint is created with correct properties", () => {
-    expect(sprint.getScrumMaster()).toBe(scrumMaster);
-    expect(sprint.getMembers()).toContain(scrumMaster);
-    expect(sprint.getMembers()).toContain(leadDeveloper);
-    expect(sprint.getStartDate()).toBe(startDate);
-    expect(sprint.getEndDate()).toBe(endDate);
-    expect(sprint.getName()).toBe(name);
+  describe("Created sprint", () => {    
+    test("If created sprint is created with correct properties", () => {
+      expect(sprint.getScrumMaster()).toBe(scrumMaster);
+      expect(sprint.getMembers()).toContain(scrumMaster);
+      expect(sprint.getMembers()).toContain(leadDeveloper);
+      expect(sprint.getBacklog()).toBe(backlog);
+      expect(sprint.getStartDate()).toBe(startDate);
+      expect(sprint.getEndDate()).toBe(endDate);
+      expect(sprint.getName()).toBe(name);
+      expect(sprint.getId()).toBe(id);
+      expect(sprint.sprintType).toBe(type);
+    });
+
+    // Test for a created sprint that you can't changeBacklogItemPosition in the sprint backlog
+    test("Created sprint can't change backlog item position", () => {
+      const backlogItem = new BacklogItem("Backlog item 5", "This is a backlog item", 5);
+      sprint.addBacklogItem(backlogItem);
+      const todoList = sprint.getTodoList();
+      const doingList = sprint.getDoingList();
+
+      sprint.changeBacklogItemPosition(developer, backlogItem, todoList, doingList)
+      expect(sprint.getDoingList()).not.toContain(backlogItem);
+    });
+
+    // Test for a created sprint that you can only edit the name, start date, end date
+    test("Created sprint can be edited", () => {
+      const newStartDate = new Date("2023-03-25");
+      const newEndDate = new Date("2023-04-29");
+      const newName = "Sprint 2";
+      sprint.setStartDate(newStartDate);
+      sprint.setEndDate(newEndDate);
+      sprint.setName(newName);
+      expect(sprint.getStartDate()).toBe(newStartDate);
+      expect(sprint.getEndDate()).toBe(newEndDate);
+      expect(sprint.getName()).toBe(newName);
+    });
+
+    // Test in a created sprint that you can add backlog items to the sprint backlog
+    test("Created sprint can add backlog items", () => {
+      const backlogItem = new BacklogItem("Backlog item 1", "This is a backlog item", 5);
+      sprint.addBacklogItem(backlogItem);
+      const todoList = sprint.getTodoList();
+      expect(todoList.getBacklogItems()).toContain(backlogItem);
+    });
+
+    // A created sprint that has a end date that is less than the time right now should be finished
+    test("Created sprint that has a end date that is less than the time right now should be finished", () => {
+      const newEndDate = new Date("2020-04-29");
+      sprint.setEndDate(newEndDate);
+      sprint.start(scrumMaster);
+      expect(sprint.getState() instanceof FinishedState).toBe(true);
+    });
+
+    // Cannot close a sprint that is not started
+    test("Cannot close a sprint that is not started", () => {
+      sprint.close();
+      expect(sprint.getState() instanceof FinishedState).toBe(false);
+    });
+
+    // Test for a created sprint that can remove a backlog item from the sprint backlog
+    // test("Created sprint can remove backlog items", () => {
+    //   const backlogItem = new BacklogItem("Backlog item 1", "This is a backlog item", 5);
+    //   sprint.addBacklogItem(backlogItem);
+    //   sprint.removeBacklogItem(backlogItem);
+    //   const todoList = sprint.getTodoList();
+    //   expect(todoList.getBacklogItems()).not.toContain(backlogItem);
+    // });
+
+    test("sprint can be finished", () => {
+      sprint.start(scrumMaster);
+      sprint.finish();
+      expect(sprint.getState() instanceof FinishedState).toBe(true);
+    });
   });
 
-  test("sprint can be finished", () => {
-    sprint.start(scrumMaster);
-    sprint.finish();
-    expect(sprint.getState() instanceof FinishedState).toBe(true);
+  describe("Activated sprint", () => { 
+    // Test for an activated sprint that you can't add backlog items to the sprint backlog
+    test("Activated sprint can't add backlog items", () => {
+      const backlogItem = new BacklogItem("Backlog item 1", "This is a backlog item", 5);
+      sprint.start(scrumMaster);
+      sprint.addBacklogItem(backlogItem);
+      const todoList = sprint.getTodoList();
+      expect(todoList.getBacklogItems()).not.toContain(backlogItem);
+    });
+
   });
 
-  // add more tests as needed...
 });
